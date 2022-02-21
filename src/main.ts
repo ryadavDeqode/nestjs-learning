ba958572160e80d568bc44cb1b42340a897e09e2
ryadavDeqode <ryadav@deqode.com>
2022-02-21 22:23:56 +0530

    Added Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const APP_NAME = process.env.npm_package_name;
+  const APP_VERSION = process.env.npm_package_version;
+
+  const options = new DocumentBuilder()
+    .setTitle(APP_NAME)
+    .setDescription(`The ${APP_NAME} API description`)
+    .setVersion(APP_VERSION)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
